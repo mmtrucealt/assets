4eebda979993e78a8723c142f3fb95223309d1b9
mmtrucealt <158788027+mmtrucealt@users.noreply.github.com>
2024-08-09 16:54:52 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 console.log('This file should be linked to every file. If this is not the case, then it is considered a bug.');
+
+// site checker
+(function() {
+    var identifier = document.createElement('div');
+    identifier.id = 'assets-check';
+    identifier.style.display = 'none';
+    identifier.innerText = 'Site is up';
+    document.body.appendChild(identifier);
+})();
